a98a2186ebabda8155d6489acae327147dd1fd81
ConsolidaRegalis88888 <91187766+ConsolidaRegalis88888@users.noreply.github.com>
2023-05-04 10:43:55 +0300

    This commit includes changes to the front-end and back-end code, removing unnecessary database files, and improving the overall code structure.1

---
+++ server/controllers/fakeContacts.js
@@ -0,0 +1,35 @@
+const contacts = [
+    { id: 1, name: 'John Smith', company: 'Twitter, Inc', street: '795 Folsom Ave,Suite 600',
+        city: 'San Francisco CA 94107', telephone: '(123)456-7890', photo: "https://iili.io/H8q3KW7.jpg",
+        job_title: 'Graphics designer', gpsLatitude:0, gpsLongitude:0 },
+
+    { id: 2, name: 'Michael Zimber', company: 'Twitter, Inc', street: '795 Folsom Ave,Suite 600',
+        city: 'San Francisco CA 94107', telephone: '(123)456-7890', photo: "https://iili.io/H8q3JR4.jpg",
+        job_title: 'Sales manager', gpsLatitude:0, gpsLongitude:0 },
+
+    { id: 3, name: 'Alex Jonathan', company: 'Twitter, Inc', street: '795 Folsom Ave,Suite 600',
+        city: 'San Francisco CA 94107', telephone: '(123)456-7890', photo: "https://iili.io/H8q3FxS.jpg",
+        job_title: 'CEO', gpsLatitude:0, gpsLongitude:0 },
+
+    { id: 4, name: 'Alex Jonathan', company: 'Twitter, Inc', street: '795 Folsom Ave,Suite 600',
+        city: 'San Francisco CA 94107', telephone: '(123)456-7890', photo: "https://iili.io/H8q3FxS.jpg",
+        job_title: 'CEO', gpsLatitude:0, gpsLongitude:0 },
+
+    { id: 5, name: 'Sandra Smith', company: 'Twitter, Inc', street: '795 Folsom Ave,Suite 600',
+        city: 'San Francisco CA 94107', telephone: '(123)456-7890', photo: "https://iili.io/H8q3fs9.jpg",
+        job_title: 'Graphics designer', gpsLatitude:0, gpsLongitude:0 },
+
+    { id: 6, name: 'John Smith', company: 'Twitter, Inc', street: '795 Folsom Ave,Suite 600',
+        city: 'San Francisco CA 94107', telephone: '(123)456-7890', photo: "https://iili.io/H8q3KW7.jpg",
+        job_title: 'Graphics designer', gpsLatitude:0, gpsLongitude:0 },
+
+    { id: 7, name: 'Monica Smith', company: 'Twitter, Inc', street: '795 Folsom Ave,Suite 600',
+        city: 'San Francisco CA 94107', telephone: '(123)456-7890', photo: "https://iili.io/H8q3KW7.jpg",
+        job_title: 'Marketing manager', gpsLatitude:0, gpsLongitude:0 },
+
+    { id: 8, name: 'Janet Carton', company: 'Twitter, Inc', street: '795 Folsom Ave,Suite 600',
+        city: 'San Francisco CA 94107', telephone: '(123)456-7890', photo: "https://iili.io/H8q3dOl.jpg",
+        job_title: 'Graphics designer', gpsLatitude:0, gpsLongitude:0 },
+];
+
+module.exports={contacts}
